0a3b6c6bbc3db82a8e8a9bcefdd0658a8785afad
HeyyD <heidi.hati@cs.tamk.fi>
2019-01-19 17:15:13 +0200

    new component audio-controls

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import { AudioControlsComponent } from '../components/audio-controls/audio-controls.component';
 
 @NgModule({
   imports: [
@@ -18,6 +19,6 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, AudioControlsComponent]
 })
 export class HomePageModule {}
